2646416aeeda36e4a43f5826fa34022a85ebb5d5
mana <mana@hippo.space>
2022-08-11 17:39:32 +0800

    silence "Not Implemented" test failures

--- src/utils.rs
+++ src/utils.rs
@@ -109,7 +109,7 @@ pub fn generate_jest_config() -> (String, String) {
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testPathIgnorePatterns: ["dist/*"],
+  testPathIgnorePatterns: ["dist/*", "aptos_framework/voting", "aptos_framework/stake", "aptos_std/signature", "aptos_framework/bucket_table"],
 };
 "###;
     ("jest.config.js".to_string(), content.to_string())
