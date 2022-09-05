6647690c31101a0284767a9a2b5dbd8dc232a4b4
mana <mana@hippo.space>
2022-09-05 20:49:18 +0800

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ use move_package::compilation::package_layout::CompiledPackageLayout;
 use move_package::source_package::layout::SourcePackageLayout;
 use shared::{Context, MoveToTsOptions};
 use std::collections::BTreeMap;
-use std::path::{Path};
+use std::path::Path;
 use std::process;
 use std::rc::Rc;
 
