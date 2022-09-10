68444d8ab76f98017f169ededf9e051c154c0c9a
mana <mana@hippo.space>
2022-09-10 08:07:53 +0800

    [broken] upgrade to aptos 1.3.12

--- src/utils.rs
+++ src/utils.rs
@@ -65,9 +65,9 @@ pub fn generate_package_json(package_name: String, cli: bool, ui: bool) -> (Stri
     "typescript": "^4.6.4"
   }},
   "dependencies": {{
-    "aptos": "1.3.11",
+    "aptos": "1.3.12",
     "big-integer": "^1.6.51",{}
-    "@manahippo/move-to-ts": "^0.2.16"
+    "@manahippo/move-to-ts": "^0.2.18"
   }}
 }}
 "###,
