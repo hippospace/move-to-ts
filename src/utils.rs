95c6c3a2d1a0a72074944748645867ddf9fc1b7a
mana <mana@hippo.space>
2023-03-01 11:48:21 +0800

    version bumps

--- src/utils.rs
+++ src/utils.rs
@@ -67,7 +67,7 @@ pub fn generate_package_json(package_name: String, cli: bool, ui: bool) -> (Stri
   "dependencies": {{
     "aptos": "1.6.0",
     "big-integer": "^1.6.51",{}
-    "@manahippo/move-to-ts": "^0.5.6"
+    "@manahippo/move-to-ts": "^2.0.0"
   }}
 }}
 "###,
